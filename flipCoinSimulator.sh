c7d164accae086b65eac68d04ecc59d0e6262ab2
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-12 18:22:23 +0530

    flipping coin and calculated result

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
-echo "welcome"
-read -p "how much time you flip coin " number
-read -p "how many coins " coin
+echo "Welcome to Flip The Coin"
+read -p "How many times do you want to flip the coin : " flipCoin
+read -p "How many coins( 1, 2 or 3 ) : " coin
 declare -A result
 
 headCount=0
@@ -12,26 +12,25 @@ headHead=0
 tailHead=0
 HeadTail=0
 
-function coin()
-{
-	for (( i=1; i<=$number; i++ ))
-	do
-	s=""
- 	for(( j=0; j<$coin; j++ ))
- 	do
-    		resultk=$(( RANDOM % 2 ))
+function coin() {
+for (( i=1; i<=$flipCoin; i++ ))
+do
+s=""
+ for(( j=0; j<$coin; j++ ))
+ do
+    resultk=$(( RANDOM % 2 ))
 
-      		if [ $resultk == 1 ]
-		then
-   			s=$s"H"
-           		headCount=$(( $headCount + 1 ))
-		else
-            		s=$s"T"
-           		tailCount=$(( $tailCount + 1))
-		fi
- 	done
-	echo final key : $s
-	result["$s"]=$(( ${result["$s"]} + 1 ))  
+      if [ $resultk == 1 ]
+then
+   s=$s"H"
+           headCount=$(( $headCount + 1 ))
+else
+            s=$s"T"
+           tailCount=$(( $tailCount + 1))
+fi
+ done
+echo final key : $s
+result["$s"]=$(( ${result["$s"]} + 1 ))  
 
 done
 }
