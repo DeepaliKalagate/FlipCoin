29570ba4c687791ded6d6e9c737922ce1cc3c887
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-12 16:11:18 +0530

    Flipping coin multiple times and calculated percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,14 +1,37 @@
-#!/bin/bash
+#!/bin/bash -x
+
+read -p "Enter number of times to flip the coin" number
 
 isHead=0
 isTail=1
+headsCount=0
+tailsCount=0
+declare -A resultDictionary
 
-check=$(( RANDOM%2 ))
+function calculation()
+{
+	for (( i=1; i<=$1; i++ ))
+	do
+		check=$(( RANDOM%2 ))
 
-if [ $check -eq $isHead ]
-then
-	echo "heads"
-else
-	echo "tails"
-fi
+		if [ $check -eq $isHead ]
+		then
+			headsCount=$((headsCount+1))
+			resultDictionary[Heads]=$headsCount
+		else
+			tailsCount=$((tailsCount+1))
+			resultDictionary[Tails]=$tailsCount
+		fi
+	done
+}
 
+function calculatePercentage()
+{
+	head=$(( $headsCount*100 ))
+	headPercentage=$(( $head/$1 ))
+	tail=$(( $tailsCount*100 ))
+	tailsPercentage=$(( $tail/$1 ))
+}
+
+calculation $number
+calculatePercentage $number
