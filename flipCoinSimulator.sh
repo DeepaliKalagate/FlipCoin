a3f163644f3af4bbc734fa0143643603993f1da5
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-13 12:47:42 +0530

    Flipped coin and calculates percentage and sorted values

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,24 +4,24 @@ tailcount=0
 declare -A flipCoinDictionary
 function flipCoinCalculator
 {
-echo $1
+	echo $1
 }
 read -p "How many number of times you want to flip coin" numberOfFlips
 read -p "Enter how many number of coins you want to flip" numberOfCoins
 for(( j=1; j<=$numberOfFlips; j++ ))
 do
-key=""
-for(( i=1; i<=$numberOfCoins; i++ ))
-do
-result="$( flipCoinCalculator $((RANDOM%2)) )"
-if [ $result -eq 1 ]
-then
-key=$key"H"
-headcount=$(( $headcount + 1 ))
-else
-key=$key"T"
-tailcount=$(( $tailcount + 1))
-fi
+	key=""
+	for(( i=1; i<=$numberOfCoins; i++ ))
+	do
+		result="$( flipCoinCalculator $((RANDOM%2)) )"
+		if [ $result -eq 1 ]
+		then
+			key=$key"H"
+			headcount=$(( $headcount + 1 ))
+		else
+			key=$key"T"
+			tailcount=$(( $tailcount + 1))	
+		fi
 done
 flipCoinDictionary["$key"]=$(( ${flipCoinDictionary["$key"]} + 1 ))
 echo "final key" $key
